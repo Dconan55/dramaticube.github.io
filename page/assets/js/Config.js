9f2605cd39ffe7d3b486e835d4984d3eb4b011d1
Dconan55 <73534998+Dconan55@users.noreply.github.com>
2022-02-12 04:48:02 +0100

    Add files via upload

---
+++ page/assets/js/Config.js
@@ -0,0 +1,59 @@
+const Config = {
+/* START CONFIG */
+
+'server-logo': 'assets/img/logo.png',
+
+'server-ip': 'dramaticsurvie.craft.gg',
+
+'server-status': {
+	'background-color': '#fff659',
+	'text-color': 'black',
+	'icon': 'assets/img/status.png',
+
+	'value': '{online} / {max} Joueurs connecté !',
+	'copy-value': 'IP COPIE!'
+},
+
+
+'background': {
+	'type': 'image', // TYPES: IMAGE OR COLOR
+	'value': "assets/img/OIP.png",
+	'opacity': {
+		'color': 'black',
+		'value': 0.8
+	}
+},
+
+
+'buttons-style': {
+	'background-color': '#fff659',
+	'text-color': 'black'
+},
+
+'web-buttons': {
+	'Store': {
+		icon: 'assets/img/store.png',
+		url: 'https://store.myweb.net'
+	},
+	'Site Web': {
+		icon: 'assets/img/forum.png',
+		url: 'https://www.dramaticube.fr'
+	},
+	'Bans': {
+		icon: 'assets/img/bans.png',
+		url: 'https://bans.myweb.net'
+	}
+}
+
+
+
+
+/* END CONFIG */
+};
+
+
+
+
+
+/* IGNORE */
+export default Config;
